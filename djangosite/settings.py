30309eb444a859288af66e169863f1a7d7d460c9
lzq <18248392627@163.com>
2017-04-21 15:26:12 +0800

    123wwwddd

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -91,11 +91,23 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'medical_basic',
+        'USER': 'root',
+        'PASSWORD': os.getenv('DBPASSWD'),
+        'HOST': os.getenv('DBHOST'),
+        'PORT': '3306',
     }
+	
 }
 
 # Internationalization
