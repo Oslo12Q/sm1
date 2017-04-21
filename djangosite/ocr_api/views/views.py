545be1b33df1689e7c04cbfa483788b7d93b2732
lzq <18248392627@163.com>
2017-04-21 13:48:40 +0800

    ddddddddddddddd

--- djangosite/ocr_api/views/views.py
+++ djangosite/ocr_api/views/views.py
@@ -63,7 +63,9 @@ def async_analysis_result(request):
     rsp_data = data_clear(file_dest)
     indicators, extra_info, unknown_indicators = rsp_data.get('indicators', []), rsp_data.get('extra_info', {}), rsp_data.get('unknown_indicators', [])
     result = dict(indicators=indicators, extra_info=extra_info)
-    return get_json_response(request, dict(status='ok', message='success.', data=result))
+    result_data = json.dumps(result)
+    print result_data
+    return get_json_response(request, dict(status='ok', message='success.', data=result_data))
 
     # if request.GET.get('type') == 'info':
     #     return get_json_response(request, dict(status='ok', message='success', data=dict(doc_path='/api/ocr/async_analysis/result?fid=%s' % file_id)))
