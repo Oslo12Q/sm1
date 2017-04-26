fe9141ecf0325c952f1039144a499f49353a9239
lzq <18248392627@163.com>
2017-04-26 15:29:12 +0800

    11

--- djangosite/ocr_api/admin.py
+++ djangosite/ocr_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#from models.models import *
+
+
+#admin.site.register(Useradmin)
--- djangosite/settings.py
+++ djangosite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'sm.djangosite.ocr_api'
+    'sm.djangosite.ocr_api',
 )
 
 MIDDLEWARE_CLASSES = (
